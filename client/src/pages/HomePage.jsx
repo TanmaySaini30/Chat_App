bb79da08fe779da4ec05522a61efeb7d872b54d6
Tanmay Saini <apple@apples-MacBook-Air-8.local>
2026-06-06 00:34:21 +0530

    Fix duplicate import and configure server listener

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
-import { useContext } from 'react'
 import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 
-if(process.env.NODE_ENV !== "production"){
+if(!process.env.VERCEL){
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, ()=> console.log("Server is running on PORT: "+PORT));
 }
